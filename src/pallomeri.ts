6f4fa3d979bd36248de427282889c6c22128e089
Riku Kokko <riku.kokko@futurice.com>
2019-05-24 17:05:18 +0300

    Add walls. move balls to origin. draw axis if needed. add space walls and ground.

--- src/pallomeri.ts
+++ src/pallomeri.ts
@@ -23,14 +23,15 @@ const createScene = function() {
     // Create a FreeCamera, and set its position to {x: 0, y: 5, z: -10}
     const camera = new BABYLON.FreeCamera(
         'camera1',
-        new BABYLON.Vector3(18, 20, (0.5 * NUMBER_OF_ELEMS) / ROW_SIZE),
+        new BABYLON.Vector3(0, 20, 0),
         scene,
     );
     // Target the camera to scene origin
     const center = new BABYLON.Vector3(
-        18,
         0,
-        (0.5 * NUMBER_OF_ELEMS) / ROW_SIZE,
+        0,
+        0,
+        // (0.5 * NUMBER_OF_ELEMS) / ROW_SIZE,
     );
     camera.setTarget(center);
     // Attach the camera to the canvas
@@ -43,11 +44,15 @@ const createScene = function() {
     );
 
     const groundMaterial = new BABYLON.StandardMaterial('mat', scene);
-    groundMaterial.emissiveTexture = new BABYLON.Texture('grass.png', scene);
-    groundMaterial.diffuseColor = BABYLON.Color3.White();
+    groundMaterial.emissiveTexture = new BABYLON.Texture(
+        '1024px-Hubble_ultra_deep_field_high_rez_edit1.jpg',
+        scene,
+    );
+    groundMaterial.diffuseColor = BABYLON.Color3.Black();
     // Ground
     let ground = BABYLON.Mesh.CreateBox('Ground', 1, scene);
-    ground.scaling = new BABYLON.Vector3(100, 1, 100);
+
+    ground.scaling = new BABYLON.Vector3(50, 1, 50);
     ground.position.y = 0;
     ground.checkCollisions = true;
     ground.material = groundMaterial;
@@ -55,9 +60,67 @@ const createScene = function() {
     ground.physicsImpostor = new BABYLON.PhysicsImpostor(
         ground,
         BABYLON.PhysicsImpostor.BoxImpostor,
-        { mass: 0, friction: 0.5, restitution: 0.7 },
+        { mass: 0, friction: 1, restitution: 0 },
         scene,
     );
+
+    // Walls
+    var border0 = BABYLON.Mesh.CreateBox('border0', 1, scene);
+    border0.scaling = new BABYLON.Vector3(1, 100, 100);
+    border0.position.y = -5.0;
+    border0.position.x = -25.0;
+    border0.checkCollisions = true;
+
+    var border1 = BABYLON.Mesh.CreateBox('border1', 1, scene);
+    border1.scaling = new BABYLON.Vector3(1, 100, 100);
+    border1.position.y = -5.0;
+    border1.position.x = 25.0;
+    border1.checkCollisions = true;
+
+    var border2 = BABYLON.Mesh.CreateBox('border2', 1, scene);
+    border2.scaling = new BABYLON.Vector3(100, 100, 1);
+    border2.position.y = -5.0;
+    border2.position.z = 25.0;
+    border2.checkCollisions = true;
+
+    var border3 = BABYLON.Mesh.CreateBox('border3', 1, scene);
+    border3.scaling = new BABYLON.Vector3(100, 100, 1);
+    border3.position.y = -5.0;
+    border3.position.z = -25.0;
+    border3.checkCollisions = true;
+
+    border0.material = groundMaterial;
+    border1.material = groundMaterial;
+    border2.material = groundMaterial;
+    border3.material = groundMaterial;
+
+    border0.physicsImpostor = new BABYLON.PhysicsImpostor(
+        border0,
+        BABYLON.PhysicsImpostor.BoxImpostor,
+        { mass: 0 },
+        scene,
+    );
+    border1.physicsImpostor = new BABYLON.PhysicsImpostor(
+        border1,
+        BABYLON.PhysicsImpostor.BoxImpostor,
+        { mass: 0 },
+        scene,
+    );
+    border2.physicsImpostor = new BABYLON.PhysicsImpostor(
+        border2,
+        BABYLON.PhysicsImpostor.BoxImpostor,
+        { mass: 0 },
+        scene,
+    );
+    border3.physicsImpostor = new BABYLON.PhysicsImpostor(
+        border3,
+        BABYLON.PhysicsImpostor.BoxImpostor,
+        { mass: 0 },
+        scene,
+    );
+
+    ground.receiveShadows = true;
+
     let elements: BABYLON.Mesh[] = [];
     for (let j = 0; j < NUMBER_OF_LAYERS; j++) {
         for (let i = 0; i < NUMBER_OF_ELEMS; i++) {
@@ -70,20 +133,25 @@ const createScene = function() {
                 false,
                 BABYLON.Mesh.FRONTSIDE,
             );
-            sphere.position.x = (i % ROW_SIZE) + Math.random() - 0.1;
-            sphere.position.z = Math.floor(i / ROW_SIZE) + Math.random() - 0.1;
-            sphere.position.y = Math.random() * 2 + 2 * j;
+            sphere.position.x =
+                (i % ROW_SIZE) + Math.random() - 0.1 - ROW_SIZE / 2;
+            sphere.position.z =
+                Math.floor(i / ROW_SIZE) + Math.random() - 0.1 - 10;
+            // sphere.position.y = Math.random() * 2 + 2 * j;
+            // sphere.position.x = i % ROW_SIZE;
+            // sphere.position.z = Math.floor(i / ROW_SIZE);
+            sphere.position.y = 2 * j;
 
             const mat = new BABYLON.StandardMaterial('mat', scene);
             mat.emissiveTexture = new BABYLON.Texture('grass.png', scene);
-            mat.diffuseColor = BABYLON.Color3.Random();
+            mat.diffuseColor = BABYLON.Color3.Yellow();
             sphere.material = mat;
 
             // physics
             sphere.physicsImpostor = new BABYLON.PhysicsImpostor(
                 sphere,
                 BABYLON.PhysicsImpostor.SphereImpostor,
-                { mass: 1, restitution: 0.1 },
+                { mass: 1, restitution: 0 },
                 scene,
             );
 
@@ -101,41 +169,33 @@ document.querySelector('button')!.addEventListener('click', function() {
     this.hidden = true;
     init();
     const { scene, elements } = createScene();
+    // showAxis(5, scene);
     let numberOfLoops = 0;
     // run the render loop
     engine.runRenderLoop(() => {
-        numberOfLoops++;
-        if (numberOfLoops >= 2) {
-            numberOfLoops = 0;
-            const audioIntensity = getCurrentIntensity();
-            const bassIntesity =
-                (audioIntensity[0] + audioIntensity[1] + audioIntensity[2]) / 3;
-            const impulseVector = new BABYLON.Vector3(
-                0,
-                (bassIntesity * 3) / 100,
-                0,
-            );
-            elements.forEach((elem) => {
-                const position = elem.getAbsolutePosition();
-                if (position.y <= 1) {
-                    elem.physicsImpostor!.applyImpulse(impulseVector, position);
-                }
-            });
-        }
+        // numberOfLoops++;
+        // if (numberOfLoops >= 2) {
+        //     numberOfLoops = 0;
+        const audioIntensity = getCurrentIntensity();
+        const bassIntesity =
+            (audioIntensity[0] + audioIntensity[1] + audioIntensity[2]) / 3;
+        const impulseVector = new BABYLON.Vector3(
+            0,
+            (bassIntesity * 3) / 100,
+            0,
+        );
+        elements.forEach((elem) => {
+            const position = elem.getAbsolutePosition();
+            if (position.y <= 1 && position.y >= 0) {
+                elem.physicsImpostor!.applyImpulse(impulseVector, position);
+            }
+        });
+        // }
 
         scene.render();
     });
 });
 // call the createScene function
-
-// setTimeout(() => {
-//   const impulseVector = new BABYLON.Vector3(0, 10, 0);
-//   elements.forEach((elem) => {
-//   elem.physicsImpostor.applyImpulse(
-//     impulseVector,
-//     elem.getAbsolutePosition()
-// })
-// )}, 1000);
 
 // the canvas/window resize event handler
 window.addEventListener('resize', function() {
